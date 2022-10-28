43b1f26b1acb5f1c87186d8f450c9d4495846b2d
hoon-hoon <kc0393@naver.com>
2022-10-28 17:25:11 +0900

    header 1.0

--- cosmoa-front/src/components/head/Header.js
+++ cosmoa-front/src/components/head/Header.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Box, Grid } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { AppBar, Box, Button, Grid, Toolbar, IconButton, Typography } from "@material-ui/core";
+// import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
+
     return (
       <>
-        <Grid container justifyContent="flex-end">
-          <Grid item>
-            홍길동님 내정보 /
-            {/* <Link href="/login">로그아웃</Link> */}
-          </Grid>
-          <i class="fa-solid fa-bars"></i>
-        </Grid>
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            Menu
+            {/* <MenuIcon /> */}
+          </IconButton>
+          CosMoa
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          </Typography>
+          <Button color="inherit"><a href="/login"></a></Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
         <Grid item xs={12}>
-            <Box bgcolor="primary.main"><a href="/login">Login</a></Box>
         </Grid>
       </>
     );
