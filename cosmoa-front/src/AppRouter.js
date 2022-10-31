d418546c240e23df76c168707189d63332cefeec
dbgusWkd <105102224+dbgusWkd@users.noreply.github.com>
2022-10-31 10:43:39 +0900

    footer 내용 추가, registerplace 화면이동

--- cosmoa-front/src/AppRouter.js
+++ cosmoa-front/src/AppRouter.js
@@ -7,31 +7,37 @@ import Header from "./components/head/Header";
 import Footer from "./components/footer/Footer";
 import Main from "./page/main/Main";
 import Login from "./page/login/Login";
+import RegisterPlace from "./page/register/RegisterPlace";
+import RegisterCourse from "./page/register/RegisterCourse";
+import RegisterType from "./page/register/RegisterType";
 
 
-class AppRouter extends React.Component {
-  render() {
+function AppRouter (props) {
     return (
       <BrowserRouter>
-        <Box mt={5}>
+        {/* <Box mt={5}> */}
           <Header />
-        </Box>
+        {/* </Box> */}
         <div>
           <Routes>
             <Route path="/" element={<Main />} />
             
             <Route path="/main" element={<Main />} />
             <Route path="/login" element={<Login />} />
+
+            <Route path="/registertype" element={<RegisterType />} />
+            <Route path="/registercourse" element={<RegisterCourse />} />
+            <Route path="/registerplace" element={<RegisterPlace />} />
+
           </Routes>
         </div>
         <div>
-          <Box mt={5}>
+          {/* <Box mt={5}> */}
             <Footer />
-          </Box>
+          {/* </Box> */}
         </div>
       </BrowserRouter>
     );
   }
-}
 
 export default AppRouter;
--- cosmoa-front/src/page/Register/RegisterPlace.js
+++ cosmoa-front/src/page/Register/RegisterPlace.js
@@ -1,8 +1,35 @@
 import React from "react";
+import { Button, Box, Link } from "@material-ui/core";
 
-function RegisterPlace() {
+function RegisterPlace(props) {
+
+    const fileInput = React.useRef(null);
+    
+    const handleButtonClick = e => {
+        fileInput.current.click();
+    };
+
+    const handleChange = e => {
+        console.log(e.target.files[0]);
+    };
+
     return(<>
-        Register Place Page
+        
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}><h1>Register Place Page</h1></Box>
+        <img src="../images/test.jpg" ></img>
+
+        <Button onClick={handleButtonClick} variant="contained">이미지 업로드</Button>
+        <input type="file"
+            ref={fileInput}
+            onChange={handleChange}
+            style={{ display: "none" }} />
+
+            <br />
+            <br />
+        
+        <Link href="/main">
+            <Button variant="contained">메인으로</Button>
+        </Link>
     </>)
 }
 
