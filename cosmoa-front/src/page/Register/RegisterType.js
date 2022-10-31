bab2e0cd64b7f91e811168d64b823c8d622d9372
dbgusWkd <105102224+dbgusWkd@users.noreply.github.com>
2022-10-31 12:16:30 +0900

    registerType

--- cosmoa-front/src/page/Register/RegisterType.js
+++ cosmoa-front/src/page/Register/RegisterType.js
@@ -5,11 +5,13 @@ function RegisterType() {
     return(<>
 
         <br />
+        <br />
+        <br />
 
         <h1>
             Select Register Course / Place
         </h1>
-        
+
         <Link href="/registercourse">
             <Paper elevation={4}
             style={{height:150, lineHeight:"150px", fontSize:24, width:"90%", margin:"0 auto"}}>
