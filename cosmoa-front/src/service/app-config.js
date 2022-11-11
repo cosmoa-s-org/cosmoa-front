9c367d96f89023a00038d26424fb9ac69afafe9a
dbgusWkd <105102224+dbgusWkd@users.noreply.github.com>
2022-11-11 10:02:19 +0900

    app config

--- cosmoa-front/src/service/app-config.js
+++ cosmoa-front/src/service/app-config.js
@@ -4,4 +4,5 @@ if (hostname === "localhost") {
     
 }
 backendHost = "http://14.45.203.199:8080";
+export const GOOGLE_KEY = "AIzaSyCF2fSRjBD8-27-6vd4ZuuSszDZS1oou4w";
 export const API_BASE_URL = `${backendHost}`;
