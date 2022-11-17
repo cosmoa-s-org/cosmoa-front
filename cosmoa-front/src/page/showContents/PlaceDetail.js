d18ad49372cbfd2350f486bd8e2771584b5fdd3e
km7865 <km78655@gmail.com>
2022-11-17 15:27:13 +0900

    장소 상세조회 초기 데이터 셋팅

--- cosmoa-front/src/page/showContents/PlaceDetail.js
+++ cosmoa-front/src/page/showContents/PlaceDetail.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { call } from "../../service/ApiService";
 
 function PlaceDetail() {
+    const [place, setPlace] = useState({});
+    const [comments, setComments] = useState([]);
+    const params = useParams();
+    const pid = params.id;
+
+    useEffect(() => {
+        call(`/place/detail/${pid}`, "GET", {}, null)
+        .then((response) => {
+            console.log(response);
+            setPlace(place);
+        });
+
+        call(`/place-reply/${pid}`, "GET", {}, null)
+        .then((response) => {
+            console.log(response);
+            setComments(response.data);
+        });        
+    }, []);
+
+    
+
     return(<>
-    <br />
-    <br />
-    <br />
-        
-        <h1>Place Detail Page</h1>
-        
     </>)
 }
 
