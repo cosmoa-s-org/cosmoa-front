246740bcf98f25daafac3a6d2d31de29368fb8d7
dbgusWkd <105102224+dbgusWkd@users.noreply.github.com>
2022-11-17 16:20:21 +0900

    navigate site

--- cosmoa-front/src/page/Register/RegisterType.js
+++ cosmoa-front/src/page/Register/RegisterType.js
@@ -1,31 +1,61 @@
 import React from "react";
 import { Paper, Link } from "@material-ui/core";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function RegisterType() {
-    return(<>
+  const M = window.M;
+  const location = useLocation();
+  // const history = useHistory();
+  let navigate = useNavigate();
 
-        <h1>
-            Select Register Course / Place
-        </h1>
+  M.onBack(() => {
+    if (["/signin", "/main"].include(location.path)) {
+      // signin, main 화면에서 뒤로가기 버튼 클릭시
+      if (window.confirm("앱을 종료하시겠습니까?")) M.sys.exit();
+    } else {
+      navigate.back();
+    }
+  });
 
-        <Link href="/registercourse">
-            <Paper elevation={4}
-            style={{height:150, lineHeight:"150px", fontSize:24, width:"90%", margin:"0 auto"}}>
-                코스 등록
-            </Paper>
-        </Link>
+  return (
+    <>
+      <h1>Select Register Course / Place</h1>
 
-        <br />
+      <Link href="/registercourse">
+        <Paper
+          elevation={4}
+          style={{
+            height: 150,
+            lineHeight: "150px",
+            fontSize: 24,
+            width: "90%",
+            margin: "0 auto",
+          }}
+        >
+          코스 등록
+        </Paper>
+      </Link>
 
-        <Link href="/registerplace">
-            <Paper elevation={4}
-            style={{height:150, lineHeight:"150px", fontSize:24, width:"90%", margin:"0 auto"}}>
-                장소 등록
-            </Paper>
-        </Link>
+      <br />
 
-        <br />
-    </>)
+      <Link href="/registerplace">
+        <Paper
+          elevation={4}
+          style={{
+            height: 150,
+            lineHeight: "150px",
+            fontSize: 24,
+            width: "90%",
+            margin: "0 auto",
+          }}
+        >
+          장소 등록
+        </Paper>
+      </Link>
+
+      <br />
+    </>
+  );
 }
 
 export default RegisterType;
--- cosmoa-front/src/page/main/Main.js
+++ cosmoa-front/src/page/main/Main.js
@@ -1,9 +1,10 @@
 import { Box, Container, Link, Paper } from "@material-ui/core";
 //import { MenuIcon } from "@material-ui/icons";
 import * as React from 'react';
+import { useNavigate } from "react-router-dom";
 
 function Main() {
-
+  const navigate = useNavigate;
 
   return (<>
     
@@ -11,12 +12,13 @@ function Main() {
       <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}><h1>MainPage</h1></Box>
     </Container>
 
-    <Link href="/registertype">
+    {/* <Link href="/registertype"> */}
       <Paper variant="contained"
-            style={{height:150, lineHeight:"150px", fontSize:24, width:"90%", margin:"0 auto", backgroundColor:"#B1DCF8"}}>
+            style={{height:150, lineHeight:"150px", fontSize:24, width:"90%", margin:"0 auto", backgroundColor:"#B1DCF8"}}
+            onClick={ () => { navigate("/registertype")} }>
             코스 및 장소 등록
       </Paper>
-    </Link>
+    {/* </Link> */}
 
     <br />
 
