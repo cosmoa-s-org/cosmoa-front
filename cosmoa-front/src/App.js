daaf852f0b762507d0c41abc50362a7724603dbb
dbgusWkd <105102224+dbgusWkd@users.noreply.github.com>
2022-11-20 17:33:21 +0900

    how to splash

--- cosmoa-front/src/App.js
+++ cosmoa-front/src/App.js
@@ -1,14 +1,41 @@
-import { useState } from 'react';
-import './App.css';
-import AppRouter from './AppRouter';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./App.css";
+import AppRouter from "./AppRouter";
+import Splash from "./components/Splash";
 
 function App() {
+  const [time,setTime] = useState(<Splash />);
+  const location = useLocation();
 
-return (<>
-    <div className="App" >
-      <AppRouter />
-    </div>
-    </>);
+  // useEffect(()=>{
+  //   if(location.pathname==="/"){
+  //     setTimeout(function() {
+  //       setTime(<AppRouter />)
+  //     }, 3000)}
+  //   else if(location.pathname==="/signin"){
+  //     setTimeout(function() {
+  //       setTime(<AppRouter />)
+  //     }, 3000)
+  //   }
+  //   else{setTime(<AppRouter />)}
+  // },[])
+
+  return (
+    <>
+      <div className="App">
+        
+      {time}
+      
+      {
+        setTimeout(function() {
+          setTime(<AppRouter />)
+        }, 3000)
+      }
+
+      </div>
+    </>
+  );
 }
 
 export default App;
