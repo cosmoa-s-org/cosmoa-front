c61cc3a0d3b2a940ad17be1a6820909773de8f64
dbgusWkd <105102224+dbgusWkd@users.noreply.github.com>
2022-11-24 13:30:58 +0900

    main, lotteworld

--- cosmoa-front/src/components/footer/Footer.js
+++ cosmoa-front/src/components/footer/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     // 화면 아래 고정적으로 출력할 footer (copyright())
     return (<>
     <div style={{position:"fixed", bottom:0, width:"100vw", backgroundColor:"white", color:"black"}}>
-        <Typography>금오공과대학교 cosmoa팀</Typography>
+        <Typography>금오공과대학교 COSMOA</Typography>
     </div>
     </>)
 }
