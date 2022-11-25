acc881b0ce611ac609771d55352b4130fe99b3bb
km7865 <rjsxo789@naver.com>
2022-11-25 15:53:39 +0900

    코스목록조회 스타일 수정 폰트 크기 변경 및 마진 추가

--- cosmoa-front/src/page/showContents/CourseList.js
+++ cosmoa-front/src/page/showContents/CourseList.js
@@ -80,7 +80,7 @@ function CourseList() {
         paddingLeft: "0", 
         paddingTop: "0", 
         textAlign: "left", 
-        fontSize: "18px", 
+        fontSize: "15px", 
         fontWeight: "normal", 
         fontFamily: "fantasy"
     };
@@ -146,22 +146,22 @@ function CourseList() {
                     </Grid>
                     <Grid item xs={7}>
                         <Grid container spacing={2}>
-                            <Grid item xs={12} style={{height: "25px", lineHeight: "15px", fontWeight: "bolder", }}>
+                            <Grid item xs={12} style={{height: "25px", lineHeight: "15px", fontWeight: "bolder", marginRight: "15px",  }}>
                                 {item.course.name}
                             </Grid>
-                            <Grid item xs={12} style={{height: "60px", marginBottom: "0", paddingBottom: "0", marginTop: "0", paddingTop: "0"}}>
-                                <p style={{height: "16px", lineHeight: "16px", paddingRight: "15px", marginTop: "5px", }}>
-                                    {item.course.description.slice(0, 40) + " ..."}
+                            <Grid item xs={12} style={{height: "55px", marginBottom: "0", paddingBottom: "0", marginTop: "5px", paddingTop: "0"}}>
+                                <p style={{height: "16px", lineHeight: "16px", paddingRight: "15px", marginTop: "5px", fontSize: "10px", }}>
+                                    {item.course.description.slice(0, 50) + " ..."}
                                 </p>
                             </Grid>
-                            <Grid item xs={3} style={{height: "25px", marginTop: "0", marginRight: "0", paddingTop: "0px", }}>
-                                <ThumbUpIcon />
+                            <Grid item xs={3} style={{height: "25px", marginTop: "0", marginRight: "0", paddingTop: "1px", }}>
+                                <ThumbUpIcon style={{height: "17px"}} />
                             </Grid>
                             <Grid item xs={3} style={countStyle}>
                                 {item.course.like}
                             </Grid>
-                            <Grid item xs={3} style={{height: "25px", marginTop: "0", marginRight: "0", paddingTop: "2px", }}>
-                                <CommentIcon />
+                            <Grid item xs={3} style={{height: "25px", marginTop: "0", marginRight: "0", paddingTop: "3px", }}>
+                                <CommentIcon style={{height: "17px"}} />
                             </Grid>
                             <Grid item xs={3} style={countStyle}>
                                 {item.course.replyCount}
