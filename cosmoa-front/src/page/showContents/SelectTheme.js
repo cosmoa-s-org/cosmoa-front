b5720b5ac1f86662daa4b56f58e786c5e556ae92
dbgusWkd <105102224+dbgusWkd@users.noreply.github.com>
2022-11-25 15:23:03 +0900

    change select theme Screen Config

--- cosmoa-front/src/page/showContents/SelectTheme.js
+++ cosmoa-front/src/page/showContents/SelectTheme.js
@@ -1,102 +1,181 @@
 import React from "react";
-import { Grid, Link, Paper, Container, Box, Typography } from "@material-ui/core";
+import {
+  Grid,
+  Link,
+  Paper,
+  Container,
+  Box,
+  Typography,
+} from "@material-ui/core";
 import { Stack, styled } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { call } from "../../service/ApiService";
-import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import GroupIcon from '@mui/icons-material/Group';
+import CloudIcon from '@mui/icons-material/Cloud';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import LandscapeIcon from '@mui/icons-material/Landscape';
+import AttractionsIcon from '@mui/icons-material/Attractions';
 
-const PaperItem = styled(Paper)((({theme}) => ({
-    backgroundColor: "lightsalmon",
-    height:150,
-    width: 150,
-    lineHeight:"150px", 
-    fontSize:24,
-    margin:"0 auto",
-    float:"left",
-    color:"white",
-    borderRadius:"20px",
-    margin: "1vw",
-    fontWeight:"700"
-})))
+const PaperItem = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#A6D7F7",
+  height: 80,
+  width: "66vw",
+  lineHeight: "85px",
+  fontSize: 24,
+  // margin: "0 auto",
+  float: "left",
+  color: "white",
+  borderRadius: "20px",
+  margin: "1vw",
+  marginLeft: "8vw",
+  fontWeight: "700",
+}));
 
 const Item = styled(Paper)(({ theme }) => ({
-    // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    // ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    marginTop: "5px",
-    fontWeight: "700"
+  // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  // ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  marginTop: "5px",
+  fontWeight: "700",
+}));
+
+const IconItem = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#A6D7F7",
+  height: 80,
+  width: 80,
+  borderRadius: "35px",
+  margin: "1vw",
+  lineHeight: "110px",
 }));
 
 function SelectTheme() {
+  const header = { "Content-Type": "application/json" };
+  const navigate = useNavigate();
+  const [likeCourse, setLikeCourse] = useState([]);
 
-    const header = { "Content-Type": "application/json" };
-    const navigate = useNavigate();
-    const [likeCourse, setLikeCourse] = useState([]);
-  
-    useEffect(() => {
-      call("/course/hot", "GET", header, null)
-        .then((response) => {
-          console.log(response);
-          setLikeCourse(response.data);
-        })
-        }, []);
-  
+  useEffect(() => {
+    call("/course/hot", "GET", header, null).then((response) => {
+      console.log(response);
+      setLikeCourse(response.data);
+    });
+  }, []);
 
-    const likeClick = (id) => {
-        console.log(id);
-        navigate(`/coursedetail/${id}`);
-      }
+  const likeClick = (id) => {
+    console.log(id);
+    navigate(`/coursedetail/${id}`);
+  };
 
-    return(<>
-        
-        <Container maxWidth="sm">
-            <br />
-        <Paper style={{width:"100%", marginTop: "5px", backgroundColor:"#FFF89A", fontWeight:700}}>인기 코스</Paper>
-          <Item style={{ backgroundColor: "#B1DCF8", width:"100%" }} onClick={() => {likeClick(likeCourse[0].id)}}>
-            <AutoAwesomeIcon style={{color: "yellow"}} />{likeCourse.length > 0 ? likeCourse[0].name : ""}<AutoAwesomeIcon style={{color: "yellow"}} />
+  return (
+    <>
+      <Container maxWidth="sm">
+        <br />
+        <Paper
+          style={{
+            width: "100%",
+            height: "40px",
+            marginTop: "5px",
+            marginBottom: "5px",
+            backgroundColor: "#FFF89A",
+            fontWeight: 700,
+            fontSize: "28px",
+          }}
+        >
+          인기 코스
+        </Paper>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Item
+              style={{ backgroundColor: "#B1DCF8", width: "100%" }}
+              onClick={() => {
+                likeClick(likeCourse[0].id);
+              }}
+            >
+              {/* <AutoAwesomeIcon style={{ color: "yellow" }} /> */}
+              {likeCourse.length > 0 ? likeCourse[0].name : ""}
+              {/* <AutoAwesomeIcon style={{ color: "yellow" }} /> */}
             </Item>
-        <Item style={{ backgroundColor: "#B1DCF8", width:"100%" }} onClick={() => {likeClick(likeCourse[1].id)}}>
-        <AutoAwesomeIcon style={{color: "yellow"}} />{likeCourse.length > 0 ? likeCourse[1].name : ""}<AutoAwesomeIcon style={{color: "yellow"}} />
-        </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item
+              style={{ backgroundColor: "#B1DCF8", width: "100%" }}
+              onClick={() => {
+                likeClick(likeCourse[1].id);
+              }}
+            >
+              {/* <AutoAwesomeIcon style={{ color: "yellow" }} /> */}
+              {likeCourse.length > 0 ? likeCourse[1].name : ""}
+              {/* <AutoAwesomeIcon style={{ color: "yellow" }} /> */}
+            </Item>
+          </Grid>
+        </Grid>
       </Container>
-        <br />
-        <Grid  spacing={2} style={{display:"inline-block", padding:"5px"}}>
+      <br />
 
-        <Link href="/courselist">
-            <PaperItem elevation={4}
-            style={{}}>
-                구성 인원
-            </PaperItem>
-        </Link>
-
-        <Link href="/">
-            <PaperItem elevation={4}>
-                계절 및 기간
-            </PaperItem>
-        </Link>
-
-        <br />
-
-        <Link href="/">
-            <PaperItem elevation={4}>
-                지역 축제
-            </PaperItem>
-        </Link>
-
-        <Link href="/">
-            <PaperItem elevation={4}>
-                추천 명소
-            </PaperItem>
-        </Link>
-
+      <Grid container spacing={1} style={{ padding: "5px", marginLeft:10 }}>
+        <Grid item xs={2}>
+          <Link href="/courselist">
+            <IconItem elevation={4}>
+              <GroupIcon style={{color:"white", fontSize: 40,}}/>
+            </IconItem>
+          </Link>
+        </Grid>
+        <Grid item xs={10}>
+          <Link href="/courselist">
+            <PaperItem elevation={4}>구성 인원</PaperItem>
+          </Link>
         </Grid>
 
         <br />
-        
-    </>)
+
+        <Grid item xs={2}>
+          <Link href="/courselist">
+            <IconItem elevation={4}>
+              <CloudIcon style={{color:"white", fontSize: 40,}}/>
+            </IconItem>
+          </Link>
+        </Grid>
+        <Grid item xs={10}>
+          <Link href="/courselist">
+            <PaperItem elevation={4}>계절 및 기간</PaperItem>
+          </Link>
+        </Grid>
+
+        <br /><Grid item xs={2}>
+          <Link href="/courselist">
+            <IconItem elevation={4} style={{  lineHeight: "120px",}}>
+              <AttractionsIcon style={{color:"white", fontSize: 50}}/>
+            </IconItem>
+          </Link>
+        </Grid>
+        <Grid item xs={10}>
+          <Link href="/courselist">
+            <PaperItem elevation={4}>지역 축제</PaperItem>
+          </Link>
+        </Grid>
+
+        <br /><Grid item xs={2}>
+          <Link href="/courselist">
+            <IconItem elevation={4}>
+              <AccountBalanceIcon style={{color:"white", fontSize: 40,}}/>
+            </IconItem>
+          </Link>
+        </Grid>
+        <Grid item xs={10}>
+          <Link href="/courselist">
+            <PaperItem elevation={4}>추천 명소</PaperItem>
+          </Link>
+        </Grid>
+
+        <br />
+      </Grid>
+
+      <br />
+    </>
+  );
 }
 
 export default SelectTheme;
